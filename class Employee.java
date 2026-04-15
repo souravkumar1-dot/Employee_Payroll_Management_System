a28273c8490effed8a09083d86ac46f0e9a1f275
Sourav Kumar <sourav.kumar1@rungta.org>
2026-04-15 20:33:59 +0530

    Refactor Employee class and improve salary calculation
    Updated employee type checks to be case-insensitive and added an error message for invalid types. Changed employee names and salary parameters for demonstration.

--- class Employee.java
+++ class Employee.java
@@ -1,4 +1,4 @@
-class Employee {
+public class Employee {
     String name;
     int id;
     double salary;
@@ -9,41 +9,37 @@ class Employee {
         this.id = id;
     }
 
-    // Method to calculate salary using if
     void calculateSalary(String type, double baseSalary, double bonus, int hours, double rate) {
 
-        if (type.equals("fulltime")) {
+        if ("fulltime".equalsIgnoreCase(type)) {
             salary = baseSalary + bonus;
         } 
-        else if (type.equals("parttime")) {
+        else if ("parttime".equalsIgnoreCase(type)) {
             salary = hours * rate;
         } 
         else {
             salary = 0;
+            System.out.println("Invalid employee type");
         }
     }
 
-    // Display details
     void display() {
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
         System.out.println("Salary: " + salary);
     }
-}
 
-public class PayrollSystem {
+    
     public static void main(String[] args) {
 
-        // Full-time employee
-        Employee e1 = new Employee("Rahul", 101);
-        e1.calculateSalary("fulltime", 40000, 10000, 0, 0);
+        Employee e1 = new Employee("John Doe", 101);
+        e1.calculateSalary("fulltime", 5000, 1200, 0, 0);
         e1.display();
 
         System.out.println();
 
-        // Part-time employee
-        Employee e2 = new Employee("Amit", 102);
-        e2.calculateSalary("parttime", 0, 0, 60, 200);
+        Employee e2 = new Employee("Jane Smith", 102);
+        e2.calculateSalary("parttime", 0, 0, 6, 200);
         e2.display();
     }
 }
