edc8a2a304d166f663e8e6593ac71b4e0b71f506
Sourav Kumar <sourav.kumar1@rungta.org>
2026-06-07 00:09:31 +0530

    Refactor Employee class to use inheritance

--- class Employee.java
+++ class Employee.java
@@ -1,44 +1,52 @@
-public class Employee {
+abstract class Employee {
     String name;
     int id;
-    double salary;
-
-    Employee(String name, int id) {
+ Employee(String name, int id) {
         this.name = name;
         this.id = id;
     }
-
-    void calculateSalary(String type, double baseSalary, double bonus, int hours, double rate) {
-
-        if ("fulltime".equalsIgnoreCase(type)) {
-            salary = baseSalary + bonus;
-        } 
-        else if ("parttime".equalsIgnoreCase(type)) {
-            salary = hours * rate;
-        } 
-        else {
-            salary = 0;
-            System.out.println("Invalid employee type");
-        }
-    }
-
+    abstract double calculateSalary();
     void display() {
         System.out.println("Name: " + name);
         System.out.println("ID: " + id);
-        System.out.println("Salary: " + salary);
+        System.out.println("Salary: " + calculateSalary());
+    }
+}
+class FullTimeEmployee extends Employee {
+    double baseSalary;
+    double bonus;
+
+    FullTimeEmployee(String name, int id, double baseSalary, double bonus) {
+        super(name, id);
+        this.baseSalary = baseSalary;
+        this.bonus = bonus;
     }
 
-    
+    @Override
+    double calculateSalary() {
+        return baseSalary + bonus;
+    }
+}
+class PartTimeEmployee extends Employee {
+    int hours;
+    double rate;
+
+    PartTimeEmployee(String name, int id, int hours, double rate) {
+        super(name, id);
+        this.hours = hours;
+        this.rate = rate;
+    }
+   @Override
+    double calculateSalary() {
+        return hours * rate;
+    }
+}
+public class Main {
     public static void main(String[] args) {
-
-        Employee e1 = new Employee("John Doe", 101);
-        e1.calculateSalary("fulltime", 5000, 1200, 0, 0);
+        Employee e1 = new FullTimeEmployee("John Doe", 101, 5000, 1200);
+        Employee e2 = new PartTimeEmployee("Jane Smith", 102, 6, 200);
         e1.display();
-
         System.out.println();
-
-        Employee e2 = new Employee("Jane Smith", 102);
-        e2.calculateSalary("parttime", 0, 0, 6, 200);
         e2.display();
     }
 }
